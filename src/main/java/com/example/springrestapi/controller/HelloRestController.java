dd41cf5e6f8d3e32601acaee6d6ee25d724c1602
bharatpathak1998 <bharatpathak099@gmail.com>
2023-02-04 11:48:54 +0530

    Added : Functionality To Show Hello Messageusing Query Parameters

--- src/main/java/com/example/springrestapi/controller/HelloRestController.java
+++ src/main/java/com/example/springrestapi/controller/HelloRestController.java
@@ -1,6 +1,7 @@
 package com.example.springrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloRestController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello From BridgeLabz!!!";
+    }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
