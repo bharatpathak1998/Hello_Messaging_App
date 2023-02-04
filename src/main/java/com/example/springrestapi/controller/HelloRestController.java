d561dce9bd4905ca4595244de795f18ac1dfd7fe
bharatpathak1998 <bharatpathak099@gmail.com>
2023-02-04 12:02:20 +0530

    Added : Functionality To Show Hello Message Using Path Variable

--- src/main/java/com/example/springrestapi/controller/HelloRestController.java
+++ src/main/java/com/example/springrestapi/controller/HelloRestController.java
@@ -1,6 +1,7 @@
 package com.example.springrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class HelloRestController {
 
     @GetMapping("/query")
     public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 }
